3f7b271e06f1fe1213b892583159e48e568c4a15
Thiago de Assis <paulo.antunes@meta.com.br>
2023-03-23 16:54:01 -0300

    fix: adiciona verificação e ativa estilo ao usar teclado

--- main.js
+++ main.js
@@ -16,5 +16,16 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom(idAudio);
     }
     
-    console.log(contador);
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
 }
